f559c3dde2d2a3d7111db4289f25d4095ed1f5eb
alaaDragneel <moaalaa16@gmail.com>
2018-01-25 00:26:08 +0200

    start

---
+++ app/Thread.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+
+    public function path()
+    {
+        return url('threads/'. $this->id) ;
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class, 'thread_id');
+    }
+}

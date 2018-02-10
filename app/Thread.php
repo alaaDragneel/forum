49a24b791f92af899ab82c12774c780828268390
alaaDragneel <m.alaa@orbscope.com>
2018-02-10 19:11:18 +0200

    Till 16

--- app/Thread.php
+++ app/Thread.php
@@ -8,15 +8,19 @@ use Psy\CodeCleaner\AssignThisVariablePass;
 class Thread extends Model
 {
     protected $guarded = [];
-    
+
+    protected static function boot ()
+    {
+        parent::boot();
+
+        static::addGlobalScope('replyCount', function ($builder) {
+            $builder->withCount('replies');
+        });
+    }
+
     public function path()
     {
         return url("/threads/{$this->channel->slug}/{$this->id}");
-    }
-
-    public function replies()
-    {
-        return $this->hasMany(Reply::class, 'thread_id');
     }
 
     public function owner()
@@ -24,14 +28,24 @@ class Thread extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class, 'channel_id');
+    }
+
     public function addReply($reply) 
     {
         $this->replies()->create($reply);
     }
 
-    public function channel()
+    public function replies()
     {
-        return $this->belongsTo(Channel::class, 'channel_id');
+        return $this->hasMany(Reply::class, 'thread_id');
+    }
+
+    public function scopeFilter ($query, $filters)
+    {
+        return $filters->apply($query);
     }
 
 }
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,45 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <a href="#"> {{ $thread->owner->name }} </a> Posted: 
-                    {{ $thread->title }}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                {{-- Main Thread Section::start--}}
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <a href="#"> {{ $thread->owner->name }} </a> Posted:
+                        {{ $thread->title }}
+                    </div>
+                    <div class="panel-body">
+                        {{ $thread->body }}
+                    </div>
                 </div>
-                <div class="panel-body">
-                    {{ $thread->body }}
+                {{-- Main Thread Section::end--}}
+
+                {{-- Reply Section::start --}}
+                @foreach ($replies as $reply)
+                    @include('threads.reply')
+                @endforeach
+                {{ $replies->links() }}
+                {{-- Reply Section::end --}}
+                {{-- Reply Form Section::start --}}
+                @if (auth()->check())
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body" id="body" class="form-control" placeholder="Have Something To Say ?" rows="5"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-success">Post</button>
+                    </form>
+                @else
+                    <div class="alert alert-success" role="alert">
+                        <h4 class="alert-heading">Want Join The Discussion</h4>
+                        <p>Please <a href=" {{ route('login') }} ">Login</a> To Join This Discussion</p>
+                    </div>
+                @endif
+                {{-- Reply Form Section::end --}}
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p>
+                            This thread was published {{ $thread->created_at->diffForHumans() }}
+                            by <a href="#">{{ $thread->owner->name }}</a>,
+                            and currently has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            @foreach ($thread->replies as $reply)
-                @include('threads.reply')
-            @endforeach
-        </div>
-    </div>
-
-  <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            @if (auth()->check())
-                <form method="POST" action="{{ $thread->path() . '/replies' }}">
-                    {{ csrf_field() }}
-                    <div class="form-group">
-                        <textarea name="body" id="body" class="form-control" placeholder="Have Something To Say ?" rows="5"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-success">Post</button>
-                </form>
-            @else 
-                <div class="alert alert-success" role="alert">
-                    <h4 class="alert-heading">Want Join The Discussion</h4>
-                    <p>Please <a href=" {{ route('login') }} ">Login</a> To Join This Discussion</p>
-                </div>
-            @endif
-        </div>
-    </div>
-</div>
 @endsection
